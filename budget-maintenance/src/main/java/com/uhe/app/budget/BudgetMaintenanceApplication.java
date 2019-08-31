38d152f3322e6d71ce5b156e06c7df457fde5a2b
unknown <ulrichheeger@hotmail.com>
2019-08-31 21:52:23 +0200

    TEST

--- budget-maintenance/src/main/java/com/uhe/app/budget/BudgetMaintenanceApplication.java
+++ budget-maintenance/src/main/java/com/uhe/app/budget/BudgetMaintenanceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BudgetMaintenanceApplication {
 
 	public static void main(String[] args) {
+        System.out.println();
 		SpringApplication.run(BudgetMaintenanceApplication.class, args);
 	}
 
