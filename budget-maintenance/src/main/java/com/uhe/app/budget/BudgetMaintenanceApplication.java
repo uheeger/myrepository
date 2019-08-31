32a29a08520fe6ca6d52f2bb070257ace0a5d99e
unknown <ulrichheeger@hotmail.com>
2019-08-31 21:35:24 +0200

    TEST

--- budget-maintenance/src/main/java/com/uhe/app/budget/BudgetMaintenanceApplication.java
+++ budget-maintenance/src/main/java/com/uhe/app/budget/BudgetMaintenanceApplication.java
@@ -8,6 +8,7 @@ public class BudgetMaintenanceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetMaintenanceApplication.class, args);
+        System.out.println();
 	}
 
 }
